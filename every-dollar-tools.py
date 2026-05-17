5d9393d55b1689db047be76f56bfacc2268a2b39
Nick Toothaker <nicktoothaker@gmail.com>
2026-05-17 15:56:00 -0500

    feature: added --plan-transfers arg and function

--- every-dollar-tools.py
+++ every-dollar-tools.py
@@ -2,15 +2,17 @@ import os
 import sys
 import argparse
 import pandas as pd
+from utillities.file import pdf_to_text, clean_text_file, make_budget_file_csv
+from utillities.planner import plan_transfers
 
 parser = argparse.ArgumentParser(description='Every Dollar Tools')
 parser.add_argument('--transaction-file', type=str, help='Path to the input file (CSV format)')
 parser.add_argument('--budget', type=str, help='Path to the monthly budget PDF file')
+parser.add_argument('--plan-transfers', action='store_true', help='Plan transfers based on the budget and transactions')
 
 args = parser.parse_args()
 
 if args.budget:
-    from utillities.file import pdf_to_text, clean_text_file, make_budget_file_csv
     filepath = os.path.expanduser(args.budget)
     basename, ext = os.path.splitext(filepath)
 
@@ -29,3 +31,10 @@ if args.transaction_file:
     except Exception as e:
         print(f"Error loading file: {e}")
         sys.exit(1)
+
+if args.plan_transfers:
+    if not args.budget or not args.transaction_file:
+        print("Both --budget and --transaction-file arguments are required to plan transfers.")
+        sys.exit(1)
+    planned_transfers = plan_transfers(f"output/{basename}.csv", args.transaction_file)
+    print(planned_transfers)
